61db370ee6571bcf9e4598611eaf40ab76b7c43c
M0decondition <modecondition@yandex.com>
2025-07-27 22:34:19 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from transliterate import translit
+print(translit("Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. \nPeople have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.", 'ru'))
+print()
+print(translit("More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...", 'ru'))
+print(translit("78 - seventy-eight \n15 - fifteen \n3 - three \n40 - forty \n8 - eight", 'ru'))
